e99391e7a31869a9732ef6e6e940f872eb3d1279
Sergio <seridancar@gmail.com>
2022-09-25 13:23:50 +0100

    MSA-2: Add statistics calculator class and Apache math library

---
+++ src/main/java/com/example/microstreaminganalytics/utils/StatisticalCalculator.java
@@ -0,0 +1,55 @@
+package com.example.microstreaminganalytics.utils;
+
+import com.example.microstreaminganalytics.entity.Statistics;
+import com.sergioviera.microstreaminganalytics.entity.Statistics;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+@Component
+public class StatisticalCalculator {
+
+    private List<Integer> list;
+
+    public Statistics obtainStatisticCalculations(List<Integer> variableValues) {
+        list = variableValues;
+
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+
+        Statistics statistics = new Statistics();
+        statistics.setTimestamp(LocalDateTime.now());
+        statistics.setMean(descriptiveStatistics.getMean());
+        statistics.setMedian(descriptiveStatistics.getPercentile(50));
+        statistics.setMode(calculateMode());
+        statistics.setStandarDeviation(calculateStandarDeviation(descriptiveStatistics.getPopulationVariance()));
+        statistics.setQuarter1(descriptiveStatistics.getPercentile(25));
+        statistics.setQuarter2(descriptiveStatistics.getPercentile(50));
+        statistics.setQuarter3(descriptiveStatistics.getPercentile(75));
+        statistics.setMaxValue(descriptiveStatistics.getMax());
+        statistics.setMinValue(descriptiveStatistics.getMin());
+
+        return statistics;
+    }
+    private double calculateMode() {
+        double mode = list.stream()
+                .collect(Collectors.groupingBy(i -> i, TreeMap::new, Collectors.counting()))
+                .entrySet().stream().min((a, b) -> {
+                    if (!a.getValue().equals(b.getValue()))
+                        return b.getValue().compareTo(a.getValue());
+                    return a.getKey().compareTo(b.getKey());
+                }).get().getKey();
+
+        return mode;
+    }
+    private double calculateStandarDeviation(double variance) {
+        double standardDeviation = Math.sqrt(variance);
+        System.out.println("standarDeviation"+standardDeviation);
+        return standardDeviation;
+    }
+}
+
+
