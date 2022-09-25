77957ae48a851d6436e338ceaf77f8cb0379cbb2
Sergio <seridancar@gmail.com>
2022-09-25 12:28:46 +0100

    Initial commit

---
+++ src/main/java/com/example/microstreaminganalytics/MicroStreamingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.microstreaminganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroStreamingAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroStreamingAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microstreaminganalytics/MicroStreamingAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microstreaminganalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroStreamingAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
