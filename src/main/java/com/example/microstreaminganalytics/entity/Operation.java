1534dda18f32b5f67a598ebac435c89d6d6461f4
Sergio <seridancar@gmail.com>
2022-09-25 12:42:38 +0100

    MSA-1:Add entities

---
+++ src/main/java/com/example/microstreaminganalytics/entity/Operation.java
@@ -0,0 +1,12 @@
+package com.example.microstreaminganalytics.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Operation {
+    private Response response;
+}
